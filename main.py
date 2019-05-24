f9ab51c7b94013a462b05ba5623de679092f39d2
VasilevskiyRA <vasilevskiyra@byterg.ru>
2019-05-24 20:32:24 +0300

    add compare_faces from face_recognition, switch readNetFromCaffe to readNetFromTensorflow

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import cv2 as cv
 import dlib
 import numpy as np
+import face_recognition
+from scipy.spatial import distance
 
 
 def shape_to_np(shape, dtype="int"):
@@ -19,12 +21,22 @@ def shape_to_np(shape, dtype="int"):
 def main():
     cap = cv.VideoCapture(0)
 
-    net = cv.dnn.readNetFromCaffe('daploy.prototxt.txt',
-                                  'res10_300x300_ssd_iter_140000.caffemodel')
+    net = cv.dnn.readNetFromTensorflow('opencv_face_detector_uint8.pb', 'opencv_face_detector.pbtxt')
+    # net = cv.dnn.readNetFromCaffe('daploy.prototxt.txt', 'res10_300x300_ssd_iter_140000.caffemodel')
 
-    # detector = dlib.get_frontal_face_detector()
+    detector = dlib.get_frontal_face_detector()
     predictor = dlib.shape_predictor('shape_predictor_68_face_landmarks.dat')
     facerec = dlib.face_recognition_model_v1('dlib_face_recognition_resnet_model_v1.dat')
+
+    scarlet_image = face_recognition.load_image_file("scarlet.jpg")
+    scarlet_face_encoding = face_recognition.face_encodings(scarlet_image)[0]
+
+    known_face_encodings = [
+        scarlet_face_encoding,
+    ]
+    known_face_names = [
+        'Scarlet',
+    ]
 
     while True:
         ret, frame = cap.read()
@@ -40,24 +52,48 @@ def main():
 
         for i in range(detections.shape[2]):
             confidence = detections[0, 0, i, 2]
-            if confidence > 0.85:
+
+            if confidence > 0.95:
                 box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
                 (startX, startY, endX, endY) = box.astype("int")
 
                 cv.rectangle(flip, (startX, startY), (endX, endY),
                              (0, 0, 255), 2)
 
-                text = "{:.2f}%".format(confidence * 100)
-                y = startY - 10 if startY - 10 > 10 else startY + 10
-                cv.putText(flip, text, (startX, y),
-                           cv.FONT_HERSHEY_SIMPLEX, 0.45, (0, 0, 255), 2)
+                # text = "{:.2f}%".format(confidence * 100)
+                # y = startY - 10 if startY - 10 > 10 else startY + 10
+                # cv.putText(flip, text, (startX, y),
+                #            cv.FONT_HERSHEY_SIMPLEX, 0.45, (0, 0, 255), 2)
 
-                rect_d = dlib.rectangle(startX, startY, endX, endY)
-                shape = predictor(flip, rect_d)
+                # shape = shape_to_np(shape)
+                # for (x, y) in shape:
+                #     cv.circle(flip, (x, y), 1, (0, 255, 0), -1)
+
+                # rect_d = dlib.rectangle(startX, startY, endX, endY)
+                # shape = predictor(flip, rect_d)
                 # face_descriptor = facerec.compute_face_descriptor(flip, shape)
-                shape = shape_to_np(shape)
-                for (x, y) in shape:
-                    cv.circle(flip, (x, y), 1, (0, 255, 0), -1)
+
+                # small_frame = cv.resize(flip, (0, 0), fx=0.25, fy=0.25)
+                # face_locations = face_recognition.face_locations(flip)
+                # if face_locations:
+                #     startY, endX, endY, startX = face_locations[0]
+                #     cv.rectangle(flip, (startX, startY), (endX, endY), (0, 255, 0), 2)
+                face_encodings = face_recognition.face_encodings(flip, [(startY, endX, endY, startX)])
+
+                for face_encoding_to_check in face_encodings:
+                    matches = face_recognition.compare_faces(known_face_encodings, face_encoding_to_check,
+                                                             tolerance=0.55)
+
+                    if True in matches:
+                        print(known_face_names[matches.index(True)])
+
+                        text = "{}".format(known_face_names[matches.index(True)])
+                        y = startY - 10 if startY - 10 > 10 else startY + 10
+                        cv.putText(flip, text, (startX, y),
+                                   cv.FONT_HERSHEY_SIMPLEX, 0.45, (0, 0, 255), 2)
+                    else:
+                        known_face_encodings.append(face_encoding_to_check)
+                        known_face_names.append('Unknown {}'.format(len(known_face_encodings)))
 
         cv.imshow('frame', flip)
         if cv.waitKey(1) & 0xFF == ord('q'):
